cb7103bf433e1f0fba80f2179cb829f40e5a5171
Jolth <jolthgs@gmail.com>
2021-04-23 11:24:02 -0500

    alter table soap_controlt

--- sql/soap_controlt.sql
+++ sql/soap_controlt.sql
@@ -5,11 +5,11 @@ ALTER TABLE gps ADD COLUMN imei varchar(20);
 -- insert into gps (name, type, active, aka) VALUES ('864180038790106', 5, 't', 'TK001');
 -- UPDATE gps SET  
 
-CREATE TABLE IF NOT EXISTS sopa_controlt (
+CREATE TABLE IF NOT EXISTS soap_controlt (
   id serial PRIMARY KEY,
   vehicle_id INT NOT NULL,
   last_position_gps_id INT NOT NULL,
-  created_on TIMESTAMP NOT NULL now(),
+  created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   last_delivery TIMESTAMP,
   FOREIGN KEY (vehicle_id) REFERENCES vehiculos (id)
   --FOREIGN KEY (last_position_gps_id) REFERENCES last_positions_gps (id)
